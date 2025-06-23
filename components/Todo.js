1ba6491fc9cbb5f9dbe3792867e690c70f9b1253
kedycatsudo <dkocausta34@gmail.com>
2025-06-22 22:45:06 -0400

    needs validation

--- components/Todo.js
+++ components/Todo.js
@@ -1,7 +1,62 @@
 class Todo {
   constructor(data, selector) {
-    console.log(data);
-    console.log(selector);
+    this._data = data;
+    this._templateElement = document.querySelector(selector);
+  }
+
+  _dateCheck() {
+    this._todoDate = this._todoElement.querySelector(".todo__date");
+    if (this._data.date) {
+      this._dueDate = new Date(this._data.date);
+      if (!isNaN(this._dueDate)) {
+        this._todoDate.textContent = `Due: ${this._dueDate.toLocaleString(
+          "en-US",
+          {
+            year: "numeric",
+            month: "short",
+            day: "numeric",
+          }
+        )}`;
+      } else {
+        this._todoDate.textContent = "";
+      }
+    } else {
+      this._todoDate.textContent = "";
+    }
+  }
+
+  _generateCheckbocEl() {
+    this._todoCheckboxEl = this._todoElement.querySelector(".todo__completed");
+    this._todoLabel = this._todoElement.querySelector(".todo__label");
+    this._todoLabel.setAttribute("for", `todo-${this._data.id}`);
+    this._todoCheckboxEl.checked = this._data.completed;
+    this._todoCheckboxEl.id = `todo-${this._data.id}`;
+  }
+
+  _setEventListeners() {
+    this._todoDeleteBtn = this._todoElement.querySelector(".todo__delete-btn");
+    this._todoDeleteBtn.addEventListener("click", () => {
+      this._todoElement.remove();
+    });
+    //checkbox event listener
+    this._todoCheckboxEl.addEventListener("change", () => {
+      this._data.completed = !this._data.completed;
+    });
+  }
+
+  getView() {
+    this._todoElement = this._templateElement.content
+      .querySelector(".todo")
+      .cloneNode(true);
+
+    this._todoNameEl = this._todoElement.querySelector(".todo__name");
+
+    this._todoNameEl.textContent = this._data.name;
+    this._generateCheckbocEl();
+    this._setEventListeners();
+    this._dateCheck();
+
+    return this._todoElement;
   }
 }
 
